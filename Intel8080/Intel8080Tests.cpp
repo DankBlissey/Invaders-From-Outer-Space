5d8e5226e9d0c54223787871c0c975d846518da6
DankBlissey <dabakka4@gmail.com>
2025-11-10 08:52:06 +0000

    Added unit test for STC

--- Intel8080/Intel8080Tests.cpp
+++ Intel8080/Intel8080Tests.cpp
@@ -6,9 +6,27 @@ TEST_CASE("CMC Complement Carry", "[opcodes, carry]") {
     testCpu.init();
     testCpu.writeMem(0x000, 0x3F);
     testCpu.writeMem(0x001, 0x3F);
+    // Carry begins at 0
     REQUIRE(Intel_8080_State().with_Carry(0).with_pc(0).stateEquals(testCpu));
     testCpu.cycle();
+    // CMC flips from 0 to 1
     REQUIRE(Intel_8080_State().with_Carry(1).with_pc(1).stateEquals(testCpu));
     testCpu.cycle();
+    // CMC flips back from 1 to 0
     REQUIRE(Intel_8080_State().with_Carry(0).with_pc(2).stateEquals(testCpu));
+}
+
+TEST_CASE("STC Set Carry", "[opcodes, carry]") {
+    CPU testCpu = CPU();
+    testCpu.init();
+    testCpu.writeMem(0x000, 0x37);
+    testCpu.writeMem(0x001, 0x37);
+    // Carry begins at 0
+    REQUIRE(Intel_8080_State().with_Carry(0).with_pc(0).stateEquals(testCpu));
+    testCpu.cycle();
+    // STC sets carry to 1
+    REQUIRE(Intel_8080_State().with_Carry(1).with_pc(1).stateEquals(testCpu));
+    testCpu.cycle();
+    // STC does nothing, carry is already at 1
+    REQUIRE(Intel_8080_State().with_Carry(1).with_pc(2).stateEquals(testCpu));
 }
