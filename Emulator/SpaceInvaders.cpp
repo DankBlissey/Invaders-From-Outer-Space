b1c62ea267b6e523815d9fbf7bab05de8c3565e1
DankBlissey <dabakka4@gmail.com>
2025-12-04 13:09:40 +0000

    Set starting display resolution to 480x640

--- Emulator/SpaceInvaders.cpp
+++ Emulator/SpaceInvaders.cpp
@@ -34,7 +34,7 @@ bool init() {
         SDL_Log( "SDL could not initialize! SDL error: %s\n", SDL_GetError());
         return false;
     }
-    if (window = SDL_CreateWindow("Space Invaders", screenHeight, screenWidth, SDL_WINDOW_RESIZABLE); window == nullptr) {
+    if (window = SDL_CreateWindow("Space Invaders", 480, 640, SDL_WINDOW_RESIZABLE); window == nullptr) {
         SDL_Log("Window could not be created! SDL error: %s\n", SDL_GetError());
         return false;
     }
